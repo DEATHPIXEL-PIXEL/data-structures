f1cbec51f0549d975b7a14e386d166e14b5f5fb0
DEATHPIXEL-PIXEL <zayaankhan337@gmail.com>
2024-12-25 19:46:15 +0530

    first commit

---
+++ data_structures.py
@@ -0,0 +1,8 @@
+empty_list=[]
+print(empty_list)
+
+numbers=[1,2,3,4,5]
+print(numbers)
+
+triples=[1,2,3,4]*3
+print(triples)
---
+++ to.py
@@ -0,0 +1,8 @@
+characters=[2,3,4,5,6,7,8,8,9]
+print(len(characters))
+
+print(characters[6])
+
+print(characters[len(characters)-1])
+
+print(characters[2:6])
